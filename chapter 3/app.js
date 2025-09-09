11d7cee86b376f42e8ade205474cf0452da12e06
bhirawan.malik <bhirawanmalik000@gmail.com>
2025-09-09 02:04:36 -0700

    done

---
+++ chapter 3/app.js
@@ -0,0 +1,37 @@
+// Question # 01
+
+let myAge = "20 years"
+
+alert("I am" + " " + myAge + " " + "old.")
+
+// Question # 02
+
+let visitNumber = "20 time!"
+
+alert("You have visited this site" + " " + visitNumber)
+
+// Question # 03
+
+let birthYear = "2000"
+
+document.write("My birth year is" + " " + birthYear)
+
+document.write("<br />")
+
+document.write("Data type of my decleared variable is number.")
+
+// Question # 04
+
+let visiterName = "Jhone Doe"
+
+let productTitle = "T-shirt(s)"
+
+let productQuantity = "5"
+
+document.write("<br />")
+
+document.write("<br />")
+
+document.write("<br />")
+
+document.write("<b>" + visiterName + " " + "ordered" + " " + productQuantity + " " + productTitle + " ") + "on XYZ Clothing store."
